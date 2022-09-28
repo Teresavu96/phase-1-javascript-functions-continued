cbff7674c215cb7304f7c01e7158938032bb3d55
Anh Thu Vu <teresavu96@gmail.com>
2022-09-28 13:50:13 -0700

    commit tree c7d865c0673fd4515be205a21fdf35f0dd6385ed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function saturdayFun(someWhere = "roller-skate"){
+    return `This Saturday, I want to ${someWhere}!`
+}
+saturdayFun ()
+function mondayWork (okay = "go to the office"){
+    return `This Monday, I will ${okay}.`
+}
+function wrapAdjective (mode = "*"){
+    return function(message = "a hard worker"){
+        return `You are ${mode}${message}${mode}!`
+    }
+}
